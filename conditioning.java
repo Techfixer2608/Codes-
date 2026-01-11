ea348bd3643e450f4fb0411486aba8f90c173c99
Techfixer2608 <bhanu@1234567899>
2026-01-11 14:31:22 +0530

    Add conditioning.java implementation

---
+++ conditioning.java
@@ -0,0 +1,33 @@
+public class conditioning{
+    public static void main(String[] args){
+         int a=10;
+         if (a==10){
+             System.out.println("hello cheta.... kachda ki peti.... billi peele");
+
+        }
+
+         int count =1;
+         while(count != 5 ){ // use while when we dont know how many times the loop will run
+             System.out.println(count);
+             count++; 
+        /*
+        syntax of for loops :
+        for ( initialisation; condition; increment/decrement)
+        
+        */
+
+        // while loop 
+        /*
+          syntax:
+          while (conmdition){
+              // body 
+              }
+         */
+       for (int countfg =1; count !=5 ;count++){ // use for when we know how many times the loop will run
+               System.out.println(count); 
+        }
+        }
+
+    }
+
+}
