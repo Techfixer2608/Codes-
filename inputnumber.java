b4b45006d6763cc2a5edf117b1fddcd8063cd3f5
Techfixer2608 <bhanu@1234567899>
2026-01-11 14:36:43 +0530

    Add inputnumber.java

---
+++ inputnumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class inputnumber{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // System.out.print("Enter a number:");
+        // int number = input.nextInt();
+        // System.out.println("The number you entered is: " + number);
+
+//         String name =input.next();
+//         System.out.println(name);
+
+
+     float marks = input.nextFloat();
+     system.out.println(marks);
+
+}
+}
