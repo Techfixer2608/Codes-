547086fd71e46b72436bdf738ba2ef9dbbbfef18
Techfixer2608 <bhanu@1234567899>
2026-01-11 14:45:46 +0530

    Add loops.java

---
+++ loops.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main (String[] args){
+         
+        // Q: print numbers from 1 to 5
+  //      for (int num =1; num<=5; num+=1){ //increase the value by 1 
+     //   System.out.println(num);
+    //  } 
+    
+    // print numbers from 1 to n 
+   /*  Scanner in = new Scanner(System.in);
+    System.out.print("enter the number upto want to print number::");
+    int n = in.nextInt();
+
+    for(int num=1; num<=n; num++)*/
+    /* int num =1;
+    while ( num<=5){
+        System.out.println(num);
+        num +=1;
+        */
+    
+    // do while 
+    /*
+     do{
+
+     }while (condition);
+
+     */
+    int n=1;
+    do{
+        System.out.println(n);
+        n++;
+    } while(n<=5);  //main difference between while and do while loop is that in do while loop programme will execute once 
+
+
+    }
+
+
+    }
+ 
+
