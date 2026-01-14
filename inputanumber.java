c2679495b27793d084866b11f4c6b92d24b78060
Bhanu <bhanu2608singh@gmail.com>
2026-01-14 21:26:08 +0530

    Add comment for Scanner usage in inputanumber.java
    Added comment to clarify the purpose of the Scanner object.

---
+++ inputanumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner 
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); //used for input 
+    
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+       //int a = scan.nextInt(), b = scan.nextInt(), c = scan.nextInt(); // another way to take input in single line .
+        
+        
+       // System.out.println(a);
+       // System.out.println(b);
+       // System.out.println(c);
+       
+        System.out.println(a+ "\n" +b+ "\n" +c ); // short method to  print all three  
+        
+        
+    }
+}
